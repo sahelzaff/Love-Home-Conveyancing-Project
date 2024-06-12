f45fe4d80267a70bd0bf4aa9374d581c76f712b7
sahelzaff <sahelzaffar2@gmail.com>
2024-06-12 23:16:42 +0530

    alignment changes

--- src/Components/Hero2/Hero2.jsx
+++ src/Components/Hero2/Hero2.jsx
@@ -6,17 +6,17 @@ import '../../Responsive.css'
 const Hero2 = () => {
     return (
         <div className='relative h-auto mb-44 '>
-            <section className="relative h-[100vh] bg-center bg-fixed bg-cover bg-no-repeat brightness-110" id='mainrc' style={{ backgroundImage: `url(${assets.banner_rc})` }}>
-                <div className="absolute inset-0 bg-white bg-opacity-0 flex flex-col items-start justify-center" id='banner_rc'>
-                    <h1 className="text-[#f0532d] font-outfit text-2xl sm:text-3xl font-medium leading-tight" id='text1'>Legal Pitfalls to Avoid When</h1>
+            <section className="relative h-[90vh] bg-center bg-cover bg-no-repeat brightness-110" id='mainrc' style={{ backgroundImage: `url(${assets.banner_rc})` }}>
+                <div className="absolute inset-0 w-[45vw] bg-white bg-opacity-0 flex flex-col items-start justify-center" id='banner_rc'>
+                    <h1 className="text-[#f0532d] font-sans text-2xl sm:text-3xl font-medium leading-tight" id='text1'>Legal Pitfalls to Avoid When</h1>
 
-                    <h2 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold font-outfit py-2 text-[#f0532d]' id='text2'>Purchasing Property.</h2>
-                    <p className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-outfit font-bold leading-tight tracking-[0.38rem] text-[#f0532d]' id='text3'>Expert Advice for Peace of Mind</p>
+                    <h2 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold font-sans py-2 text-[#f0532d]' id='text2'>Purchasing Property.</h2>
+                    <p className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-sans font-medium leading-tight tracking-[0.38rem] text-[#f0532d]' id='text3'>Expert Advice for Peace of Mind</p>
                     <div className='w-24 sm:w-32 md:w-40 lg:w-44 h-[0.3rem] bg-[#f0532d] my-3 sm:my-4 md:my-5 rounded-e-md'></div>
-                    <p className='font-poppins font-normal text-sm sm:text-base md:text-lg lg:text-xl w-full sm:w-4/6  pr-0 sm:pr-10  text-white' id='text4'>Leave the hassle to me, and focus on what really matters to you. so you can chat directly with the qualified property lawyer.
+                    <p className='font-poppins font-normal text-sm sm:text-base md:text-lg lg:text-xl w-full  text-white' id='text4'>Leave the hassle to me, and focus on what really matters to you. so you can chat directly with the qualified property lawyer.
                     </p>
 
-                    <p className='font-poppins font-normal text-sm sm:text-base md:text-lg lg:text-xl w-full sm:w-4/6  pt-4 sm:pt-5  pr-0 sm:pr-10 md:pr-20 text-white' id='text5'><span className='text-[#f0532d] font-outfit ' id='text7' >Mr Robin Carter</span><br />Principal Solicitor at Love Homes Conveyancing
+                    <p className='font-poppins font-normal w-full text-white' id='text5'><span className='text-[#f0532d] font-outfit ' id='text7' >Mr Robin Carter</span><br />Principal Solicitor at Love Homes Conveyancing
                       <br />
                       <span className=''>25 years of combined experience in accounting, law, and business</span>
 
@@ -31,7 +31,7 @@ const Hero2 = () => {
                     </div>
                 </div>
             </section>
-            <div className="absolute h-[30vh] top-[96%]  left-1/2 transform -translate-x-1/2 w-2/3 bg-[#f4f4f4] p-4 sm:p-6 md:p-8 rounded-2xl box-shadow" id='consultBox'>
+            <div className="absolute h-[30vh] top-[96%]  left-1/2 transform -translate-x-1/2 w-2/3 bg-[#f4f4f4] py-2 rounded-2xl box-shadow" id='consultBox'>
                 <div className="flex flex-col justify-center items-center gap-2 sm:gap-1 md:gap-0">
                     <h2 className="font-outfit font-medium text-base sm:text-lg md:text-xl lg:text-lg xl:text-2xl">Online & In-person Consultations Available</h2>
                     <div className="flex flex-row justify-center items-center -mt-3 sm:-mt-4 md:-mt-5 gap-4 sm:gap-6 md:gap-7">
