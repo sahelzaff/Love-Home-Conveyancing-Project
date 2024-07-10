839b5aec36575cb46679d8e4160c5e9fda9da722
sahelzaff <125549773+sahelzaff@users.noreply.github.com>
2024-07-10 10:48:20 +0530

    Update BlogDetailsPage.jsx

--- src/BlogDetailsPage.jsx
+++ src/BlogDetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';'
+import React, { useState,useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar'
 import BlogDetails from './Components/BlogDetails/BlogDetails'
 import Footer from './Components/Footer/Footer'
