67346e5289f88858ea0eca00cc1655f5e3a8432b
sahelzaff <125549773+sahelzaff@users.noreply.github.com>
2024-07-31 01:31:25 +0530

    modal h-auto

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -137,7 +137,7 @@ const Homepage = () => {
       )}
 
       {showModal && (
-        <div className="modal h-screen">
+        <div className="modal h-auto">
           <div className="modal-content">
             <span className="close" onClick={closeModal}>&times;</span>
             <img src={assets.Lovehome_ad_modal} className='cursor-pointer' alt="Love Home Ad" onClick={handleModalClick} />
